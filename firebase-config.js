0c41788ed64fe85822f98a00d112937a716b4395
jamclean23 <jamclean23@gmail.com>
2023-05-15 15:46:26 -0400

    Add firebase initialization

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration object
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwrLFbR32-yE75b40XSanuTI5yuZwroRw",
+    authDomain: "photo-tagging-app-8524e.firebaseapp.com",
+    projectId: "photo-tagging-app-8524e",
+    storageBucket: "photo-tagging-app-8524e.appspot.com",
+    messagingSenderId: "253105775067",
+    appId: "1:253105775067:web:62b28fb952effb047b47fe"
+};
+
+export default firebaseConfig;
--- src/index/index.js
+++ src/index/index.js
@@ -3,6 +3,30 @@
 
 // ====== IMPORTS ======
 
+// Firebase integration
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../../firebase-config';
+import {
+    getAuth,
+    onAuthStateChanged,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+} from 'firebase/auth';
+import {
+getFirestore,
+collection,
+addDoc,
+query,
+orderBy,
+limit,
+onSnapshot,
+setDoc,
+updateDoc,
+doc,
+serverTimestamp,
+} from 'firebase/firestore';
+
 // Styling
 import css from './styles.css';
 
@@ -13,6 +37,10 @@ import { createRoot } from 'react-dom/client';
 // Main React App
 import { App } from './React/App.js';
 
+
+// ====== Initialize Firebase ======
+
+const app = initializeApp(firebaseConfig);
 
 // ====== RENDER ======
 
