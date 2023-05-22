118a02821503edacbbd542c3cf213482a14c193f
jamclean23 <jamclean23@gmail.com>
2023-05-22 15:24:23 -0400

    Add mouse dragging

--- src/index/React/components/Main/Game/Game.js
+++ src/index/React/components/Main/Game/Game.js
@@ -1,5 +1,5 @@
 import './game.css';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import waldo1 from '../../../../../assets/waldo/waldo1.jpg';
 import waldo2 from '../../../../../assets/waldo/waldo2.jpg';
 import waldo3 from '../../../../../assets/waldo/waldo3.jpg';
@@ -9,17 +9,55 @@ const waldos = [waldo1, waldo2, waldo3, waldo4];
 
 function Game (props) {
 
-    const waldoImg = useRef(getRandomWaldo());
+    const [waldoImg, setWaldoImg] = useState(getRandomWaldo());
+    const mouseDownPos = useRef({ x: 0, y: 0 });
+    const mouseDown = useRef(false);
 
     function getRandomWaldo () {
         const index = Math.floor(Math.random() * 4);
-        console.log('index: ' + index);
-
+        
         return waldos[index];
     }
 
+
+    function handleMouseDown (event) {
+        mouseDownPos.current = { x: event.screenX, y: event.screenY};
+
+        mouseDown.current = true;
+    }
+
+    function handleMouseUp (event) {
+        console.log(mouseDownPos);
+        console.log({ x: event.screenX, y: event.screenY });
+
+        if (mouseDownPos.current.x === event.screenX && mouseDownPos.current.y === event.screenY) {
+            console.log('click');
+        } else {
+            console.log('not a click');
+        }
+
+        mouseDown.current = false;
+    }
+
+    function handleMouseMove (event) {
+        if (mouseDown.current) {
+            console.log({
+                moveX: event.movementX,
+                moveY: event.movementY
+            });
+
+            // Get gameBoard and scroll it
+            const gameBoard = document.querySelector('.Game');
+            gameBoard.scrollBy(-event.movementX, -event.movementY);
+        }
+    }
+
+    function handleMouseLeave () {
+        mouseDown.current = false;
+    }
+
     return (<div className='Game'>
-        <p>game goes here</p>
+        <img onMouseLeave={handleMouseLeave} onMouseMove={handleMouseMove} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} draggable='false' className='gameBoard' src={waldoImg}/>
     </div>);
 }
 
